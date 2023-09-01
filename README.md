b0b92bd4d694431371023d85e8a29131e402df88
JIB2017 <blacuttjuan07@gmail.com>
2023-09-01 17:15:43 -0300

    navbar p.1

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Gericht-restaurant-clone
+
+Un sitio web excelente para mostrar lo mejor que tienes para ofrecer, completamente mobile responsive.
+
+## Como se hizo:
+
+1. **UI/UX Moderna:** ¿Quieres que tus potenciales clientes prefieran quedarse en tu sitio apenas llegan? Con esta landing page los usuarios no querrán irse!.
+
+2. **Animaciones suaves y agradables:** Se aplicaron animaciones suaves y agradables para cautivar a cualquiera que lo visite y darle mas vida al sitio.
+
+3. **HTML, CSS, Javascript y React:** Tecnologías fundamentales para la web 2.0 con contenido dinámico.
+
+4. **Nomenclatura BEM:** La nomenclatura BEM es una metodología para escribir un código CSS limpio y estructurado.
+
+5. **Organización de Archivos y Carpetas:** Una organización y estructura de archivos y carpetas fácil de entender para una mejor flujo de trabajo.
+
+## Lo que aprendí:
+
+**Figma a código:** Este es el seugndo de varios proyectos que tengo en mente en el que consisten en seleccionar un diseño hecho en Figma y pasarlo a código.
+
+**Diseño original:**
+https://www.figma.com/file/yvClSI9AZBRX8UaaGEByF3/Modern-UI%252FUX%253A-Gericht 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import logo from "../../assets/gericht.png";
+//import images from '../../assets/images';
+import "./Navbar.scss";
 
 function Navbar() {
+  const [mobileMenu, setMobileMenu] = useState(false);
+
   return (
-    <div>Navbar</div>
-  )
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <ul className="app__navbar-links">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#pages">Pages</a>
+        </li>
+        <li>
+          <a href="#contact">Contact Us</a>
+        </li>
+        <li>
+          <a href="#blog">Blog</a>
+        </li>
+        <li>
+          <a href="#landing">Landing</a>
+        </li>
+      </ul>
+      <div className="app__navbar-login">
+        <a href="#login">Log In / Registration</a>
+        <div />
+        <a href="#table">Book Table</a>
+      </div>
+      <div className="app__navbar-mobile">
+        <GiHamburgerMenu size={27} onClick={() => setMobileMenu(true)} />
+        {mobileMenu && (
+          <div className="app__navbar-mobile_container slide-bottom">
+            <MdOutlineRestaurantMenu size={27} onClick={() => setMobileMenu(false)} />
+            <ul className="app__navbar-mobile__links">
+              <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#pages">Pages</a>
+              </li>
+              <li>
+                <a href="#contact">Contact Us</a>
+              </li>
+              <li>
+                <a href="#blog">Blog</a>
+              </li>
+              <li>
+                <a href="#landing">Landing</a>
+              </li>
+            </ul>
+            <div className="app__navbar-mobile_login">
+              <a href="#login">Log In / Registration</a>
+              <div />
+              <a href="#table">Book Table</a>
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
