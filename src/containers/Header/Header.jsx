773e0dec4d5bb9d79065e1e45ddfb5ea476904d3
JIB2017 <blacuttjuan07@gmail.com>
2023-09-03 23:04:18 -0300

    header p.1

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import spoon from "../../assets/spoon.png";
+import welcome from "../../assets/welcome.png";
+import "./Header.scss";
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='app__header'>
+      <div className='app__header-content'>
+        <p>Chase The New Flavour</p>
+        <img src={spoon} alt="Spoon" />
+        <h1>The Key To Fine Dining</h1>
+        <p>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+        <button type='button'>Explore Menu</button>
+      </div>
+      <div className='app__header-image'>
+        <img src={welcome} alt="Welcome" />
+      </div>
+    </div>
   )
 }
 
