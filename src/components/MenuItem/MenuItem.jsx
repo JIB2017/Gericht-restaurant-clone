cc31635300e2ae93a546ea51083f0e776737c2ca
JIB2017 <blacuttjuan07@gmail.com>
2023-09-05 02:23:03 -0300

    menu p.2

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./MenuItem.scss";
 
-function MenuItem() {
+function MenuItem({ title, price, tag }) {
   return (
-    <div>MenuItem</div>
-  )
+    <div className="app__menu-item">
+      <div className="app__menu-itemUp">
+        <h5>{title}</h5>
+        <div />
+        <h5>{price}</h5>
+      </div>
+      <p>{tag}</p>
+    </div>
+  );
 }
 
-export default MenuItem
+export default MenuItem;
--- src/containers/Menu/Menu.jsx
+++ src/containers/Menu/Menu.jsx
@@ -1,30 +1,52 @@
-import React from 'react';
+import React from "react";
 import spoon from "../../assets/spoon.png";
 import menu from "../../assets/menu.png";
+import MenuItem from "../../components/MenuItem/MenuItem";
+import data from "../../constants/data";
 import "./Menu.scss";
 
 function Menu() {
   return (
-    <div className='app__menu'>
-      <div className='app__menu-title'>
+    <div className="app__menu">
+      <div className="app__menu-title">
         <h4>Menu That Fits Your Palette</h4>
         <img src={spoon} alt="spoon" />
         <h1>Today's Special</h1>
       </div>
-      <div className='app__menu-content'>
-        <div className='app__menu-content_wine'>
+      <div className="app__menu-content">
+        <div className="app__menu-content_wine">
           <h3>Wine & Beer</h3>
+          <div className="app__menu-content_wine-details">
+            {data.wines.map((wines, index) => (
+              <MenuItem
+                title={wines.title}
+                price={wines.price}
+                tag={wines.tag}
+                key={wines.title + index}
+              />
+            ))}
+          </div>
         </div>
         <img src={menu} alt="menu" />
-        <div className='app__menu-content_cocktails'>
+        <div className="app__menu-content_cocktails">
           <h3>Cocktails</h3>
+          <div className="app__menu-content_cocktails-details">
+            {data.cocktails.map((cocktails, index) => (
+              <MenuItem
+                title={cocktails.title}
+                price={cocktails.price}
+                tag={cocktails.tag}
+                key={cocktails.title + index}
+              />
+            ))}
+          </div>
         </div>
       </div>
-      <div className='app__menu-button'>
-        <button type='button'>View More</button>
+      <div className="app__menu-button">
+        <button type="button">View More</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Menu
+export default Menu;
