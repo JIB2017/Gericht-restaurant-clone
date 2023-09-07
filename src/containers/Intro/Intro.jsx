409da5bcbf10405d7e57f60a3b3a8ebf756bcd02
JIB2017 <blacuttjuan07@gmail.com>
2023-09-07 15:38:23 -0300

    Chef fix & Intro

--- src/containers/Intro/Intro.jsx
+++ src/containers/Intro/Intro.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import video from "../../assets/meal.mp4";
+import { useState } from "react";
+import { useRef } from "react";
+import "./Intro.scss";
 
 function Intro() {
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = useRef();
+
+  const handleVideo = () => {
+    setPlayVideo(!playVideo);
+    playVideo ? vidRef.current.pause() : vidRef.current.play();
+  };
+
   return (
-    <div>Intro</div>
-  )
+    <div className="app__intro">
+      <video
+        ref={vidRef}
+        src={video}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__intro-overlay">
+        <div className="app__intro-overlay_circle" onClick={handleVideo}>
+          {playVideo ? (
+            <BsFillPlayFill color="#fff" fontSize={40} />
+          ) : (
+            <BsPauseFill color="#fff" fontSize={40} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Intro
+export default Intro;
