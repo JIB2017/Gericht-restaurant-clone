3a3e5cda464bf763b9c2c541e5a6442a2c11609d
JIB2017 <blacuttjuan07@gmail.com>
2023-09-11 17:37:49 -0300

    FindUs p.1

--- src/containers/FindUs/FindUs.jsx
+++ src/containers/FindUs/FindUs.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import spoon from "../../assets/spoon.png";
+import findUs from "../../assets/findus.png"
+import "./FindUs.scss";
 
 function FindUs() {
   return (
-    <div>FindUs</div>
+    <div className='app__findus' id='contact'>
+      <div className='app__findus-content'>
+        <h4>Contact</h4>
+        <img src={spoon} alt="spoon" />
+        <h1>Find Us</h1>
+        <p>Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG</p>
+        <h5>Opening Hours</h5>
+        <p>Mon - Fri: 10:00 am - 02:00 am</p>
+        <p>Sat - Sun: 10:00 am - 03:00 am</p>
+      </div>
+      <div className='app__findus-image'>
+        <img src={findUs} alt="spoon" />
+      </div>
+    </div>
   )
 }
 
