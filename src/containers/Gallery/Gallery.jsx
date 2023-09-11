ea4896746ff9bb7f49e17f83ae3f5a3316770e15
JIB2017 <blacuttjuan07@gmail.com>
2023-09-11 05:02:41 -0300

    parallax effects in some sections

--- src/containers/Gallery/Gallery.jsx
+++ src/containers/Gallery/Gallery.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import spoon from "../../assets/spoon.png";
+import "./Gallery.scss";
 
 function Gallery() {
   return (
-    <div>Gallery</div>
-  )
+    <div className="app__gallery">
+      <div className="app__gallery-content">
+        <h4>Instagram</h4>
+        <img src={spoon} alt="spoon" />
+        <h1>Photo Gallery</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <button type="button">View More</button>
+      </div>
+      <div className="app__gallery-images"></div>
+    </div>
+  );
 }
 
-export default Gallery
+export default Gallery;
