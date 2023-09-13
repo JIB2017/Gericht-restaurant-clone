8ec5d55502781a2d504b0e60853307a846d7c866
JIB2017 <blacuttjuan07@gmail.com>
2023-09-13 03:47:41 -0300

    Added Modals and Framer Motion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,79 @@
-import React from "react";
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, Menu } from "./containers";
+import React, { useEffect, useState } from "react";
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  Menu,
+} from "./containers";
 import { Navbar } from "./components";
+import Modal from "./components/Modal/Modal";
+import { AnimatePresence } from "framer-motion";
+import { useExitIntent } from "use-exit-intent";
 
 import "./App.scss";
 
 function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isExitIntentModal, setIsExitIntentModal] = useState(false);
+
+  const { registerHandler } = useExitIntent();
+
+  const closeExit = () => {
+    setIsExitIntentModal(false);
+  };
+
+  const close = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleModal = () => {
+    if ( isExitIntentModal !== true && isModalVisible !== true ) {
+      setIsExitIntentModal(true);
+    }
+  }
+
+  registerHandler({
+    id: "myHandlerId",
+    handler: () => {
+      handleModal();
+    }
+  })
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsModalVisible(true);
+    }, 15000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <div>
+      <AnimatePresence initial={false} mode="wait" onExitComplete={() => null}>
+        {isModalVisible && !isExitIntentModal && (
+          <Modal
+            modalOpen={isModalVisible}
+            handleClose={close}
+            title="Special Offer!"
+            text="Don't miss out on our exclusive deal."
+          />
+        )}
+        {isExitIntentModal && (
+          <Modal
+            modalOpen={isExitIntentModal}
+            handleClose={closeExit}
+            title="Wait, before you go!"
+            text="We have a special offer for you."
+          />
+        )}
+      </AnimatePresence>
       <Navbar />
       <Header />
       <AboutUs />
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,53 @@
+import { motion } from "framer-motion";
+import Backdrop from "../Backdrop/Backdrop";
+
+const Modal = ({ handleClose, title, text }) => {
+  const dropIn = {
+    hidden: {
+      opacity: 0,
+      scale: 0.75,
+    },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        ease: "easeOut",
+        duration: 0.15,
+      },
+    },
+    exit: {
+      opacity: 0,
+      scale: 0.75,
+      trainsition: {
+        ease: "easeIn",
+        duration: 0.15,
+      },
+    },
+  };
+
+  return (
+    <Backdrop onClick={handleClose}>
+      <motion.div
+        onClick={(e) => e.stopPropagation()}
+        className="modal"
+        variants={dropIn}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
+        <div className="modal__content">
+          <h1 className="title">{title}</h1>
+          <p className="opensans">{text}</p>
+          <div className="modal__content-buttons">
+            <button onClick={handleClose} className="btn-close">
+              Close
+            </button>
+            <button className="btn">See Offer</button>
+          </div>
+        </div>
+      </motion.div>
+    </Backdrop>
+  );
+};
+
+export default Modal;
