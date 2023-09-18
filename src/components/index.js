5ef25d7dd899ed57cb8cbda0b69f798cb5c5ef86
JIB2017 <blacuttjuan07@gmail.com>
2023-09-18 02:44:26 -0300

    Some final fixes

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,4 @@
-import Footer from "./Footer/Footer.jsx";
 import MenuItem from "./MenuItem/MenuItem.jsx";
 import Navbar from "./Navbar/Navbar.jsx";
-import SubHeading from "./SubHeading/SubHeading.jsx";
 
-export { Footer, MenuItem, Navbar, SubHeading };
+export { MenuItem, Navbar };
